608aaf99aa61a8968938115a4048ffaf928da41a
Timur Bondarenko <tibode495@gmail.com>
2023-05-15 10:45:52 +0600

    add time range in reaction tool

--- src/views/ReactionMetricsTool.vue
+++ src/views/ReactionMetricsTool.vue
@@ -27,11 +27,26 @@ ChartJS.register(
 );
 
 type State = "RESULTS" | "TRY" | "WAIT_RED" | "WAIT_GREEN";
+type Range =
+  | "last day"
+  | "last week"
+  | "last month"
+  | "last 3 month"
+  | "all time";
+
+const Range = {
+  lastDay: "last day" as Range,
+  lastWeek: "last week" as Range,
+  lastMonth: "last month" as Range,
+  last3Month: "last 3 month" as Range,
+  allTime: "all time" as Range,
+};
 
 let state = ref<State>("RESULTS");
 let countRetries = ref(0);
 
 const testResults = ref<number[]>([]);
+const settedRange = ref<Range>(Range.lastDay);
 let lastRedStartTime: number | null = null;
 
 function getRandomInt(min: number, max: number) {
@@ -43,48 +58,6 @@ function getRandomInt(min: number, max: number) {
 const previosValues = JSON.parse(
   localStorage.getItem("reaction-results") || "{}"
 );
-
-const averageValue =
-  Object.values(previosValues).reduce((p, c) => p + c, 0) /
-  Object.values(previosValues).length;
-
-const data = {
-  labels: Object.keys(previosValues).map((k) =>
-    new Date(+k).toLocaleDateString()
-  ),
-  datasets: [
-    {
-      label: "results",
-      data: Object.keys(previosValues).map((k) => ({
-        x: new Date(+k),
-        y: previosValues[k],
-      })),
-      fill: false,
-      borderColor: "rgb(75, 192, 192)",
-      tension: 0.1,
-    },
-    {
-      label: "averageValue",
-      data: Object.keys(previosValues).map((k) => ({
-        x: new Date(+k),
-        y: averageValue,
-      })),
-      fill: false,
-      borderColor: "yellow",
-      tension: 0.1,
-    },
-  ],
-};
-const options = {
-  responsive: true,
-  scales: {
-    x: {
-      type: "time",
-      time: {},
-      min: new Date(Math.min(...Object.keys(previosValues).map((x) => +x))),
-    },
-  },
-};
 
 const countResults = () => {
   const clearedValues = testResults.value.sort().slice(1, -1);
@@ -98,14 +71,14 @@ const countResults = () => {
   localStorage.setItem("reaction-results", JSON.stringify(previosValues));
 
   testResults.value = [];
-  window.location.reload();
+  state.value = "RESULTS";
+  previosValues.value = { ...previosValues.value };
+  // window.location.reload();
 };
 
 const startTest = () => {
-  console.log(8);
   state.value = "TRY";
   countRetries.value = 0;
-  console.log(state);
 };
 
 const startTry = () => {
@@ -135,6 +108,114 @@ const endTry = () => {
     countResults();
   }
 };
+
+const getAverage = (values: number[]): number =>
+  values.reduce((p, c) => p + c, 0) / values.length;
+
+const getMinByRange = (range: Range) => {
+  const now = Date.now() - 1000 * 60 * 60 * 2;
+  let min = new Date(now - 1000 * 60 * 60 * 24);
+  if (range === Range.lastDay) {
+    min = new Date(now - 1000 * 60 * 60 * 24);
+  } else if (range === Range.lastWeek) {
+    min = new Date(now - 1000 * 60 * 60 * 24 * 7);
+  } else if (range === Range.lastMonth) {
+    min = new Date(now - 1000 * 60 * 60 * 24 * 30);
+  } else if (range === Range.last3Month) {
+    min = new Date(now - 1000 * 60 * 60 * 24 * 90);
+  } else if (range === Range.allTime) {
+    min = new Date(Math.min(...Object.keys(previosValues).map((x) => +x)));
+  }
+
+  return min;
+};
+
+const filterValuesByRange = (
+  values: Record<number, number>,
+  range: Range
+): Record<number, number> => {
+  const min = getMinByRange(range);
+  const newValues = {} as Record<number, number>;
+  Object.keys(values).forEach((k) => {
+    if (+k < min.getTime()) {
+      return;
+    }
+    newValues[+k] = values[+k];
+  });
+
+  return newValues;
+};
+
+const getDataByRange = (range: Range, _values: Record<number, number>) => {
+  const values = filterValuesByRange(_values, range);
+
+  const averageValue = getAverage(Object.values(values));
+
+  const data = {
+    labels: Object.keys(values).map((k) => new Date(+k).toLocaleDateString()),
+    datasets: [
+      ...(![Range.allTime, Range.last3Month].includes(range)
+        ? [
+            {
+              label: "results",
+              data: Object.keys(values).map((k) => ({
+                x: new Date(+k),
+                y: values[+k],
+              })),
+              fill: false,
+              borderColor: "rgb(75, 192, 192)",
+              tension: 0.1,
+            },
+          ]
+        : []),
+      {
+        label: "averageValue",
+        data: Object.keys(values).map((k) => ({
+          x: new Date(+k),
+          y: averageValue,
+        })),
+        fill: false,
+        borderColor: "yellow",
+        tension: 0.1,
+      },
+      {
+        label: "averageValue",
+        data: Object.keys(values).map((k, i) => {
+          const slice = Object.values(values).slice(
+            i,
+            i + 10 > Object.values(values).length - 1
+              ? Object.values(values).length - 1
+              : i + 10
+          );
+          const averageValue = getAverage(slice);
+          return {
+            x: new Date(+k),
+            y: averageValue,
+          };
+        }),
+        fill: false,
+        borderColor: "green",
+        tension: 0.1,
+      },
+    ],
+  };
+  return data;
+};
+
+const getOptionsByRange = (range: Range) => {
+  const min = getMinByRange(range);
+  return {
+    responsive: true,
+    scales: {
+      x: {
+        type: "time",
+        time: {},
+        min,
+        max: Date.now() + 1000 * 60 * 60 * 2,
+      },
+    },
+  };
+};
 </script>
 
 <template>
@@ -144,8 +225,19 @@ const endTry = () => {
         >Start Test</el-button
       >
       <h3>The lower the result, the better.</h3>
+      <el-select v-model="settedRange">
+        <el-option :value="Range.lastDay"> last day</el-option>
+        <el-option :value="Range.lastWeek"> last week</el-option>
+        <el-option :value="Range.lastMonth"> last month</el-option>
+        <el-option :value="Range.last3Month"> last 3 month</el-option>
+        <el-option :value="Range.allTime">all time</el-option>
+      </el-select>
       <div class="chart-container">
-        <Chart :type="'line'" :data="data" :options="options" />
+        <Chart
+          :type="'line'"
+          :data="getDataByRange(settedRange, previosValues)"
+          :options="getOptionsByRange(settedRange)"
+        />
       </div>
     </div>
     <div v-if="state === 'TRY'" class="try-container">
@@ -225,5 +317,8 @@ main {
 .chart-container {
   width: 100%;
   height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
